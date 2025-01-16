c263bfb27e4b34da1b72ff42af2780e1d8bd48e2
IamJohnTravis <dias_osman@mail.ru>
2025-01-16 12:30:40 +0600

    Update my_bot.py

--- my_bot.py
+++ my_bot.py
@@ -2,7 +2,7 @@ from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
 # Токен вашего бота
-TOKEN = 7568589896:AAF6WNjcbv0JoKujy44DsG3RtAe78JE57pU
+TOKEN = "7568589896:AAF6WNjcbv0JoKujy44DsG3RtAe78JE57pU"
 
 # Функция для обработки команды /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
