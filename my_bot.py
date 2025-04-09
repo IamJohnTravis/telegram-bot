c15b4db5c2cfdf6acdb54d17967ecb5dfea0bd96
IamJohnTravis <dias_osman@mail.ru>
2025-04-09 14:31:20 +0600

    Update my_bot.py

--- my_bot.py
+++ my_bot.py
@@ -3,6 +3,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, fil
 
 # Токен вашего бота
 TOKEN = "7568589896:AAGfc9UP9ePvk4NB_LmpmnjCcbm2Hj03OQ8"
+bot.delete_webhook()
 
 # Функция для обработки команды /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
